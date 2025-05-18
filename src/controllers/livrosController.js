de1628b8aa3d775357ce439092fe0649dbbeb3ad
Sena <joaosenadainese@gmail.com>
2025-05-18 13:54:47 -0300

    getById e patch

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -8,6 +8,20 @@ class LivroController {
     })
   }
 
+  static listarLivroPorId = (req, res) => {
+    const id = req.params.id;
+  
+    livros.findById(id, (err, livro) => {
+      if (err) {
+        res.status(400).send({ message: `${err.message} - ID inválido.` });
+      } else if (!livro) {
+        res.status(404).send({ message: 'Livro não encontrado.' });
+      } else {
+        res.status(200).send(livro);
+      }
+    });
+  };
+
   static cadastrarLivro = (req, res) => {
     let livro = new livros(req.body);
     
@@ -19,6 +33,18 @@ class LivroController {
         res.status(201).send(livro.toJSON())
       }
     })
+  }
+
+  static atualizarLivro = (req, res) => {
+    const id = req.params.id;
+
+    livros.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+      if(err) {
+        res.status(500).send({message: `${err.message} - falha ao atualizar livro.`})
+      } else {
+        res.status(200).send("Livro atualizado com sucesso")
+      }
+    })  
   }
 }
 
